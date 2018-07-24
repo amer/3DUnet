0d42dc8d02d6754b04c0659d54415c51d5025450
Amer <amer.jazaerli@gmail.com>
2018-07-25 00:28:57 +0200

    Backup

---
+++ convert_data.py
@@ -0,0 +1,11 @@
+from preprocess import convert_brats_data
+from pathlib import Path
+
+#HOME = Path.home()
+#DATA = HOME/'data/aneurysm'
+#TRAIN_DCM = DATA/'ct-512'
+#MASK_DCM =  DATA/'mask-512'
+#TRAIN_NIF = DATA/'nifti-512'
+
+#convert_brats_data(str(TRAIN_DCM),str(TRAIN_NIF))
+print("...")
---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+#!tree /home/amer/Code/3DUnetCNN/
+#sys.path.append('/home/amer/Code/3DUnetCNN/')
+#sys.path.append('/home/amer/Code/3DUnetCNN/unet3d/utils/')
+#sys.path.append('/home/amer/Code/3DUnetCNN/unet3d/model/')
+#sys.path.append('/home/amer/Code/3DUnetCNN/unet3d/')
+# sys.path.append('/home/amer/Code/3DUnetCNN/unet3d/utils/nilearn_custom_utils/')
+
+#from .patches import *
+#from .isensee2017 import *
+# from unet import *
+# from generator import *
+# from utils import *
+# from data import *
+#from nilearn_utils import crop_img, crop_img_to
